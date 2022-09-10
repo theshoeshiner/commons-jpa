189f2d0b67616daaff3e13499741c3a5aafbc53c
dcwatson84 <dcwatson84@gmail.com>
2022-09-10 10:20:25 -0400

    embedded lob type

---
+++ src/main/java/javax/persistence/TypedLob.java
@@ -0,0 +1,63 @@
+package javax.persistence;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.Lob;
+
+/**
+ * Embedded type that wraps values commonly necessary for file storage.
+ *
+ */
+@Embeddable
+public class TypedLob {
+
+	@Column
+	String mimeType;
+	
+	@Column
+	String name;
+	
+	@Column()
+	@Lob
+	byte[] data;
+	
+	public TypedLob() {}
+
+	public TypedLob(String name, String mimeType, byte[] data) {
+		super();
+		this.name = name;
+		this.mimeType = mimeType;
+		this.data = data;
+	}
+
+
+
+	public String getMimeType() {
+		return mimeType;
+	}
+
+	public void setMimeType(String mimeType) {
+		this.mimeType = mimeType;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public byte[] getData() {
+		return data;
+	}
+
+	public void setData(byte[] data) {
+		this.data = data;
+	}
+	
+	public boolean hasData() {
+		return data != null;
+	}
+	
+}
